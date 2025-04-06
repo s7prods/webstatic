3ad24c1da08b9bdbee794919d6f216f4adbe9233
shc0743 <shc0743@outlook.com>
2025-04-06 13:10:16 +0800

    fix a bug

--- Y2hjczEwMTMuY29t/static-resource/@apex/script/base.js
+++ Y2hjczEwMTMuY29t/static-resource/@apex/script/base.js
@@ -1,16 +1,14 @@
 // base.js
 
 window.addEventListener('click', function (ev) {
-    window.addEventListener('click', function (ev) {
-        if (ev.target.tagName === 'A') try {
-            const link = ev.target.href;
-            const currentDomain = window.location.hostname;
-            const linkDomain = new URL(link).hostname;
-    
-            if (linkDomain !== currentDomain) {
-                ev.preventDefault();
-                window.open(link, '_blank');
-            }
-        } catch {}
-    });
+    if (ev.target.tagName === 'A') try {
+        const link = ev.target.href;
+        const currentDomain = window.location.hostname;
+        const linkDomain = new URL(link).hostname;
+
+        if (linkDomain !== currentDomain) {
+            ev.preventDefault();
+            window.open(link, '_blank');
+        }
+    } catch {}
 });
